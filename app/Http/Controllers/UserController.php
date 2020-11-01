478553ea05736484bcbff7cb91278d7f00425442
olalla <olallana2508@hotmail.com>
2020-11-01 10:37:53 +0100

    modificado con la alerta configcontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,30 +10,38 @@ class UserController extends Controller
     return view('user.config');
     }
     public function update(Request $request)
-    { 
-        $id= \Auth::user()->id;
-       /* $validate =$this ->validate($request, [
-            'name'=>'required |string | max:255',
-            'nick'=> 'required |string| max:255 ',
-            'email'=>' required |string | email| max:255 |unique:users,nick',$id,
-        ]*/
+    {  //usuario identificado
+        $user= \Auth::user();
+        $id= $user->id;
+        //validamos el formulario 
         $validate= $this->validate($request,[
             'name' => ['required', 'string', 'max:255'],
             'nick' => ['required', 'string', 'max:255','unique:users,nick,'.$id],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.$id],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.$id], //hacemos q sea unico pero q tenga en cuenta el anterior
             
         ]);
-        
+        //recogemos todos los datos del formulario
         $name= $request->input('name');
         $nick=$request->input('nick');
         $email=$request->input('email'); 
         $direccion=$request->input('direccion');
         $localidad=$request->input('localidad');
        $provincia=$request->input('provincia');
-        /*var_dump($id);
-        var_dump($name); 
-         var_dump($direccion);
-         die();*/
+       //asignar los valores
+       $user->name=$name;
+       $user->nick=$nick;
+       $user->email=$email;
+       $user->direccion=$direccion;
+       $user->localidad=$localidad;
+       $user->provincia=$provincia;
+
+       // hacer el update a la base de datos
+       $user->update();
+
+       return redirect()-> route('config')
+                            ->with(['message'=>"Usuario actualizado"]);
+
+  
     }
     //
 }
